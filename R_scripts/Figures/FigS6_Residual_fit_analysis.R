3029d80d7e5146ad2632145bd67cd1835f96ef8a
DanielvanAs <65972802+DanielvanAs@users.noreply.github.com>
2022-07-19 13:03:14 +0200

    Update FigS6_Residual_fit_analysis.R

--- R_scripts/Figures/FigS6_Residual_fit_analysis.R
+++ R_scripts/Figures/FigS6_Residual_fit_analysis.R
@@ -87,7 +87,7 @@ df <- data.frame(
   Repeat = Repeat[,"Estimate"]*100,
   Response = CR_res$scaled_response.Estimate, #in contrast to figure 5, estimates of residual fit are used
   both = ifelse(Response[,"FDR"] < 0.05 & Repeat[,"FDR"] < 0.05, "yes","no"))
-pcor <- corr.test(df$Repeat[df$both == "yes"], df$Response[df$both=="yes"], method="pearson")
+pcor <- corr.test(df$Repeat, df$Response, method="pearson")
 
 # reorder so that genes significant in both are plotted on front of the others
 df <- df[order(df$both),]
